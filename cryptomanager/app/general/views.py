5385697b55b30c56f030cdbd5216bf33c5479dc7
Daniel <danielboone1990@gmail.com>
2021-04-12 19:34:11 +0200

    Reorganize application with the use of Flask Blueprints

--- cryptomanager/app/general/views.py
+++ cryptomanager/app/general/views.py
@@ -1,14 +1,12 @@
-from flask import render_template
+from flask import Blueprint, render_template
 from flask_login import current_user, login_required, login_user, logout_user
 #Imports from the app package 
 from ...app import app, db
 from ..models import User
 
-@app.route("/")
-@app.route("/home")
+general = Blueprint("general", __name__)
+
+@general.route("/")
+@general.route("/home")
 def home():
     return render_template("welcomescreen.html")
-
-@app.template_filter("capitalized")
-def capitalize(value):
-    return value.capitalize()
