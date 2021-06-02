477bcf33811159e78bdbd6ee12b8773f589ab341
Daniel <danielboone1990@gmail.com>
2021-06-02 21:59:56 +0200

    Bugfix BUG1 redirect to assets page when logged in

--- cryptomanager/app/general/views.py
+++ cryptomanager/app/general/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
 from flask_login import current_user, login_required, login_user, logout_user
 from ...app import app, db
 from ..models import User
@@ -9,4 +9,6 @@ general = Blueprint("general", __name__)
 @general.route("/")
 @general.route("/home")
 def home():
+    if current_user.is_authenticated:
+        return redirect(url_for('assets.get_asset'))
     return render_template("welcomescreen.html")
